eeafe37259117e79fed56ddc599b6ca428afdf3a
Dmitrii_Stashko <1028047@mail.ru>
2023-02-01 16:07:09 +1000

    Корректировка sem1/DZ/sadacha2

--- lesson_and_seminar_online/seminari/sem1/DZ/sadacha2.py
+++ lesson_and_seminar_online/seminari/sem1/DZ/sadacha2.py
@@ -2,9 +2,19 @@
 # 123 -> 6 (1 + 2 + 3)
 # 100 -> 1 (1 + 0 + 0)
 
-numb = int(input('Введите трёхзначное число: '))
-a = numb // 100 % 10
-b = numb // 10 % 10
-c= numb % 10
-sum = a + b + c
-print(f'{numb} -> {sum} ({a} + {b} + {c})')
+# numb = int(input('Введите трёхзначное число: '))
+# a = numb // 100 % 10
+# b = numb // 10 % 10
+# c= numb % 10
+# sum = a + b + c
+# print(f'{numb} -> {sum} ({a} + {b} + {c})')
+
+# или:
+n = int(input('Введите трёхзначное число: '))
+numb = n
+sum = 0
+while numb > 0:
+    ost = numb % 10
+    sum += ost
+    numb //= 10
+print(f'{n} -> {sum}')
