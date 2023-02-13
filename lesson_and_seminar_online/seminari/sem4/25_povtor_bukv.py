0e8ed67f674a64be9be3e49bc6c008ba3a1ad82a
Dmitrii_Stashko <1028047@mail.ru>
2023-02-13 11:07:35 +1000

    Добавил задачу 27_povtor_slov и изменил задачу 25_povtor_bukv.py

--- lesson_and_seminar_online/seminari/sem4/25_povtor_bukv.py
+++ lesson_and_seminar_online/seminari/sem4/25_povtor_bukv.py
@@ -63,3 +63,21 @@ for i in m_list:
         res.append(f'{i}_{d.get(i)}')
         d[i] = d.get(i) + 1
 print(res)
+
+
+# или
+# s = 'a a a b c a a d c d d'
+# s = s.split()
+# print(s)
+# result_dic = {} #dictionary в который будем класть счетчики какой символ сколько раз встречается
+# new_s = [] # новый лист в который будем класть по условию в задаче
+# for i in s:
+# if i not in result_dic: # если еще нет в словаре, первый раз
+# new_s.append(i) # кладем наш символ
+# # print(i)
+# result_dic[i] = result_dic.get(i, 0)+ 1 # с помощью метода get возвращаем в словарь наш символ искусственно добавляя 1
+# else: # если уже есть в словаре, значит пошли повторы
+# new_s.append(f'{i}_{result_dic[i]}') # кладем значение с постфиксом и значение из словаря
+# # print(f'{i}_{result[i]}')
+# result_dic[i] = result_dic.get(i, 0) + 1
+# print(*new_s)
