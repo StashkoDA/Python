5bd02178c376e52ad359a33bab55b9420869878b
Dmitrii_Stashko <1028047@mail.ru>
2023-02-18 15:07:06 +1000

    Изменил задачу sem5/35_prostoe_chislo.py

--- lesson_and_seminar_online/seminari/sem5/35_prostoe_chislo.py
+++ lesson_and_seminar_online/seminari/sem5/35_prostoe_chislo.py
@@ -6,13 +6,34 @@
 # Input: 5
 # Output: yes
 
-def prime_number(a):
-    if a != 0 and a % a == 0 and a / 1 == a:
-        return 'yes'
-    elif int(a) == 0:
-        return 'Вы ввели ноль'
-    else:
-        return 'no'
+# def prime_number(a):
+#     if a != 0 and a % a == 0 and a / 1 == a and a % a**0.5 != 0:
+#         i = 2
+#         while i < 10:
+#             if a % i != 0:
+#                 i += 1
+#             elif a == i:
+#                 i += 1
+#             else:
+#                 return 'no'
+#         return 'yes'
+#     elif int(a) == 0:
+#         return 'Вы ввели ноль'
+#     else:
+#         return 'no'
 
-n = float(input('Введите число: '))
+# n = float(input('Введите число: '))
+# print(prime_number(n))
+
+
+# или ///////////
+
+def prime_number(a):
+    num_list = list(range(2, a))
+    for i in num_list:
+        if a % i == 0:
+            return 'no'
+    return 'yes'
+
+n = int(input('Введите число: '))
 print(prime_number(n))
