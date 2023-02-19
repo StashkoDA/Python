f67d24ce4ad494fd0d0a28e87faba43d27f00c85
Dmitrii_Stashko <1028047@mail.ru>
2023-02-19 11:11:57 +1000

    Изменил задачу sem6/DZ/30_arifm_progress.py

--- lesson_and_seminar_online/sem6/DZ/30_arifm_progress.py
+++ lesson_and_seminar_online/sem6/DZ/30_arifm_progress.py
@@ -7,18 +7,27 @@
 # Вывод: 7 9 11 13 15
 
 def array(b1, dd, nn, arr):
-    i = 2
-    while i <= nn:
-        bn = b1 + (i-1)*dd
+    i = 0
+    while i < nn:
+        bn = b1 + i*dd
         arr.append(bn)
         i += 1
-    
-        
-
 
 a1 = int(input('Введите первый элемент: '))
 d = int(input('Введите разность между элементами: '))
 n = int(input('Введите кол-во элементов: '))
-arr = [a1]
+arr = []
 array(a1, d, n, arr)
 print(arr)
+
+# или /////////////////
+# a1 = int(input('Введите первый элемент: '))
+# d = int(input('Введите разность между элементами: '))
+# n = int(input('Введите кол-во элементов: '))
+# arr = []
+# for i in range(n):
+#   an = a1 + i * d
+#   arr.append(an)    
+# print(arr)    
+
+
