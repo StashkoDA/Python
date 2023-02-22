0867748409e6fa92fa3cf1a107e9dfdc2551689c
Dmitrii_Stashko <1028047@mail.ru>
2023-02-22 20:07:10 +1000

    Изменил задачу sem6/39_massivi.py

--- lesson_and_seminar_online/seminari/sem6/39_massivi.py
+++ lesson_and_seminar_online/seminari/sem6/39_massivi.py
@@ -25,3 +25,10 @@ for i in n_list:
     if i not in m_list:
         res.append(i)
 print(res)
+
+# или ////// с примером добавления условия else для информации
+
+# list1 = [int(input(f'Введите {i + 1}-е число: ')) for i in range(int(input('Введите количество элементов первого массива: ')))]
+# list2 = [int(input(f'Введите {j + 1}-е число: ')) for j in range(int(input('Введите количество элементов второго массива: ')))]
+# new_list = [el if el not in list2 else 0 for el in list1 ]
+# print(new_list)
